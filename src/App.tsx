eb6fb73bc9bc562c97baed221eea87849bfc23a9
chenjie <rokiecoder@163.com>
2020-11-06 20:27:20 +0800

    添加button测试代码

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,20 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <Button >Hello</Button>
-                <Button disabled>Hello</Button>
-                <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Large Primary</Button>
-                <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>Primary</Button>
-                <Button disabled btnType={ButtonType.Link} size={ButtonSize.Small} href="http://www.baidu.com"> Baidu
+                <Button>Default</Button>
+                <Button btnType={ButtonType.Primary}>Primary</Button>
+                <Button btnType={ButtonType.Danger}>Danger</Button>
+                <Button btnType={ButtonType.Link} href="http://www.baidu.com">Link</Button>
+                <hr/>
+                <Button btnType={ButtonType.Primary} size={ButtonSize.Small}>Primary</Button>
+                <Button btnType={ButtonType.Primary}>Primary</Button>
+                <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Primary</Button>
+                <hr/>
+                <Button btnType={ButtonType.Primary}>primary</Button>
+                <Button disabled>disabled</Button>
+                <Button btnType={ButtonType.Link} size={ButtonSize.Small} href="http://www.baidu.com" target="_blank">
                     Link</Button>
-                <Button btnType={ButtonType.Link} size={ButtonSize.Small} href="http://www.baidu.com" target="_blank"> Baidu
+                <Button disabled btnType={ButtonType.Link} size={ButtonSize.Small} href="http://www.baidu.com"> disabled
                     Link</Button>
             </header>
         </div>
