2b4b580a47e4190696bb26af78c43a5130c9d65b
chenjie <rokiecoder@163.com>
2020-11-20 15:52:12 +0800

    使用异步方式请求数据

--- src/components/AutoComplete/autoComplete.tsx
+++ src/components/AutoComplete/autoComplete.tsx
@@ -1,32 +1,50 @@
-import React, {FC, useState, ChangeEvent} from 'react';
+import React, {FC, useState, ChangeEvent, ReactElement} from 'react';
 import Input, {InputProps} from '../Input/input';
 
+
+interface DataSourceObject {
+    value: string
+}
+
+export type DataSourceType<T = {}> = T & DataSourceObject
+
 export interface AutoCompleteProps extends Omit<InputProps, 'onSelect'> {
-    fetchSuggestions: (str: string) => string[]
-    onSelect?: (item: string) => void
+    fetchSuggestions: (str: string) => DataSourceType[] | Promise<DataSourceType[]>
+    onSelect?: (item: DataSourceType) => void
+    renderOption?: (item: DataSourceType) => ReactElement
 }
 
 export const Autocomplete: FC<AutoCompleteProps> = (props) => {
-    const {value, fetchSuggestions, onSelect, ...restProps} = props;
+    const {value, fetchSuggestions, onSelect, renderOption, ...restProps} = props;
     //inputValue来自Input组件
     const [inputValue, setInputValue] = useState(value);
-    const [suggestions, setSuggestions] = useState<string[]>([]);
+    const [suggestions, setSuggestions] = useState<DataSourceType[]>([]);
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         const value = e.target.value.trim();
         setInputValue(value);
         if (value) {
             const results = fetchSuggestions(value);
-            setSuggestions(results);
+            if (results instanceof Promise) {
+                console.log('triggered');
+                results.then(data => {
+                    setSuggestions(data);
+                });
+            } else {
+                setSuggestions(results);
+            }
         } else {
             setSuggestions([]);
         }
     };
-    const handleSelect = (item: string) => {
-        setInputValue(item);
+    const handleSelect = (item: DataSourceType) => {
+        setInputValue(item.value);
         setSuggestions([]);
         if (onSelect) {
             onSelect(item);
         }
+    };
+    const renderTemplate = (item: DataSourceType) => {
+        return renderOption ? renderOption(item) : item.value;
     };
     const generateDropdown = () => {
         return (
@@ -36,7 +54,7 @@ export const Autocomplete: FC<AutoCompleteProps> = (props) => {
                         <li key={index} onClick={() => {
                             handleSelect(item);
                         }}>
-                            {item}
+                            {renderTemplate(item)}
                         </li>
                     );
                 })}
